40b2f5b9e744870d11d4c11a829669c5f65ef039
balukondamuri <balukondamuri@gmail.com>
2025-10-23 10:24:10 -0400

    updated playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,6 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
      baseURL: process.env.BASE_URL,
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
